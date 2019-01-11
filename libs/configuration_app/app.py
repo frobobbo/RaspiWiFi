52663c11c5197a4425972cc1af0ba1da7b6a04c3
frobobbo <johnsobd@gmail.com>
2019-01-10 19:07:23 -0500

    Add Country Code to wpa_supplicant

--- libs/configuration_app/app.py
+++ libs/configuration_app/app.py
@@ -58,6 +58,7 @@ def scan_wifi_networks():
 def create_wpa_supplicant(ssid, wifi_key):
     temp_conf_file = open('wpa_supplicant.conf.tmp', 'w')
 
+    temp_conf_file.write('country=US\n')
     temp_conf_file.write('ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n')
     temp_conf_file.write('update_config=1\n')
     temp_conf_file.write('\n')
@@ -69,7 +70,7 @@ def create_wpa_supplicant(ssid, wifi_key):
     else:
         temp_conf_file.write('	psk="' + wifi_key + '"\n')
 
-    temp_conf_file.write('	}')
+    temp_conf_file.write('}')
 
     temp_conf_file.close
 
